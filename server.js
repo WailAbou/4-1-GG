eb955a50a6e288a295cc5dd7fc4f9f8f7d274d81
Wail Abou <abou.w@hotmail.com>
2022-10-16 20:28:24 +0200

    - Moved folders

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+
+app.use(express.static(__dirname + '/dist/gate-escape'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/<app-name>/index.html'));
+        '/dist/gate-escape/index.html'));
 });
+
 app.listen(process.env.PORT || 8080);
